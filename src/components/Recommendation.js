a6399032c573805636b264171f03df5cca127017
Fredrik Jurgell <fj222qp@student.lnu.se>
2024-11-02 13:55:52 +0100

    Fix dark stylign

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -15,7 +15,7 @@ export default function Recommendation() {
   }
 
   return (
-    <section className="w-full max-w-4xl my-10 p-6 md:p-10 bg-white rounded-lg shadow-xl transform transition-all duration-700 dark:bg-gray-800 dark:text-gray-200">
+    <section className="w-full max-w-4xl my-10 p-6 md:p-10 bg-white rounded-lg shadow-xl transform transition-all duration-700 dark:bg-gray-900 dark:text-gray-200">
       <h2 className="text-4xl font-extrabold mb-6 dark:text-yellow-400">Recommendations</h2>
       <div className="flex justify-center">
         <Image
